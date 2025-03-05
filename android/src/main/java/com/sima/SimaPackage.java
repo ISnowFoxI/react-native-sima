52cb07d97f7aa923e5d6d4c690f9750e680cbe1f
ISnowFoxI <sfdev22@gmail.com>
2025-03-05 22:50:02 +0400

    android build fix

--- android/src/main/java/com/sima/SimaPackage.java
+++ android/src/main/java/com/sima/SimaPackage.java
@@ -1,6 +1,5 @@
 package com.sima;
 
-import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 
@@ -8,12 +7,9 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.TurboReactPackage;
-import com.margelo.nitro.core.HybridObject;
-import com.margelo.nitro.core.HybridObjectRegistry;
 import com.margelo.nitro.sima.SimaOnLoad;
 
 import java.util.HashMap;
-import java.util.function.Supplier;
 
 public class SimaPackage extends TurboReactPackage {
   @Nullable
